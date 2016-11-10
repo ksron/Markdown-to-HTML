e67a6940b6ad97730fc3e558cce89d91b20a83f2
DaheeCha_Joy <ekgmlekgml20@naver.com>
2016-11-10 12:34:22 +0900

    continue work

--- MDconverter/src/mdconverter/option_input.java
+++ MDconverter/src/mdconverter/option_input.java
@@ -1,6 +1,5 @@
 package mdconverter;
 
-import java.io.File;
 import java.util.HashMap;
 
 public class option_input {
@@ -22,17 +21,8 @@ public class option_input {
 			System.out.println("Help message");
 			System.exit(1);
 		}
-		else {
-			try{
-				if(!inputFileExistance(args[0]))
-					throw new IllegalArgumentException("Not a valid file name:" + args[0]);
-				else
-					System.out.println("Input file selected : " + args[0]);	
-			}catch(IllegalArgumentException e){
-				System.err.println(e);
-				System.exit(0);
-			}
-		}
+		else 
+			System.out.println("Input file selected : " + args[0]);
 		
 		//Iterate args & store options to a Hashmap
 		try{
@@ -43,12 +33,6 @@ public class option_input {
 						if(!validOption(args[i]))
 							throw new IllegalArgumentException("Not a valid argument: " + args[i]);
 						else if(args[i].equalsIgnoreCase("-1")){
-							System.out.println("Option set selected");
-						}
-						else if(args[i].equalsIgnoreCase("-2")){
-							System.out.println("Option set selected");
-						}
-						else if(args[i].equalsIgnoreCase("-3")){
 							System.out.println("Option set selected");
 						}
 						{
@@ -68,29 +52,15 @@ public class option_input {
 			System.exit(0);
 		}
 		
-		//check the output file already exists
-		 //1. specified format
-		   //1-1. specified directory
-		outputFileExistence(args[0], optsList.get("f"), optsList.get("d"));
-		   //1-2. not specified directory
-		outputFileExistence(args[0], optsList.get("f"), "");
-		 //2. not specified format
-		  //2-1. specified directory
-		outputFileExistence(args[0], "html", optsList.get("d"));
-		  //2-2. not specified directory
-		outputFileExistence(args[0], "html", "");
-		
-		 //print for checking
+		//print for checking
 		for( String key : optsList.keySet() ){
             System.out.println( String.format("option : %s, args : %s", key, optsList.get(key)) );
         }
 
 	}
 	
-	//test if the options are valid. 
-	//the works for valid options are done in other section. 
 	private static boolean validOption(String arg){
-		if(arg.length() > 2 )
+		if(arg.length() > 2)
 			return false;
 		switch(arg.toLowerCase().charAt(1)){
 			case 'o':
@@ -105,25 +75,6 @@ public class option_input {
 				break;				
 		}
 		return false;
-	}
-	
-	private static boolean inputFileExistance(String inFile){
-		/*File file = new File(inFile);	
-		if(file.exists() && file.getName().endsWith("md"))
-			return true;*/
-		File file = new File(inFile+"md");
-		if(file.exists())
-			return true;
-		else
-			return false;
-	}
-	
-	private static boolean outputFileExistence(String outFile, String format, String dir){
-		File file = new File(outFile);
-		if(file.exists() && file.getName().endsWith(format))
-			return false;
-		else
-			return true;
 	}
 
 }
