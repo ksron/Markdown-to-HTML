356e60ea4562c83ebc10ea7e0b2a1766314fd466
moojoo <kasrlmoojoo@gmail.com>
2016-11-11 09:21:10 +0900

    Option input

--- MDconverter/src/mdconverter/option_input.java
+++ MDconverter/src/mdconverter/option_input.java
@@ -1,12 +1,16 @@
 package mdconverter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class option_input {
 
 	public static void main(String[] args) {
 		HashMap<String, String> optsList = new HashMap<>();
-		
+
 		//Check if there is any argument
 		if(args.length == 0){
 			System.err.println("No argument");
@@ -21,8 +25,19 @@ public class option_input {
 			System.out.println("Help message");
 			System.exit(1);
 		}
-		else 
-			System.out.println("Input file selected : " + args[0]);
+		
+		File inputFile = new File(args[0]);
+		if(!inputFile.exists()){
+			System.err.println("File does not exist:" + args[0]);
+			System.exit(-1);
+		}
+		System.out.println("Input file selected : " + args[0]);	
+		
+		//Set initial options
+		optsList.put("o", inputFile.getName().split("\\.")[0]);
+		optsList.put("f", "html");
+		optsList.put("s", "plain");
+		optsList.put("d", ".");
 		
 		//Iterate args & store options to a Hashmap
 		try{
@@ -32,10 +47,16 @@ public class option_input {
 						// --[option] currently not allowed
 						if(!validOption(args[i]))
 							throw new IllegalArgumentException("Not a valid argument: " + args[i]);
-						else if(args[i].equalsIgnoreCase("-1")){
-							System.out.println("Option set selected");
+						else if(args[i].equals("-1")){
+							System.out.println("Option set 1 selected");
 						}
-						{
+						else if(args[i].equals("-2")){
+							System.out.println("Option set 2 selected");
+						}
+						else if(args[i].equals("-3")){
+							System.out.println("Option set 3 selected");
+						}
+						else{
 							// [-option] should be followed by details
 							if((i >= args.length - 1) || args[i+1].charAt(0) == '-')
 								throw new IllegalArgumentException("Expected info after " + args[i]);
@@ -52,17 +73,44 @@ public class option_input {
 			System.exit(0);
 		}
 		
-		//print for checking
+		//check the output file already exists
+		File outputFile = new File(optsList.get("d")+ "\\" +optsList.get("o")+ "." +optsList.get("f"));
+		try {
+			if(optsList.get("d").length() > 1)
+				outputFile.getParentFile().mkdirs();
+			
+			//creating new file
+			if(!outputFile.createNewFile()){
+				//File already exist
+				Scanner reader = new Scanner(System.in);
+				System.err.println("The output file already exists. Press y to continue");
+				String s = reader.next();
+				if(s.equalsIgnoreCase("y")){
+					outputFile.delete();
+					outputFile.createNewFile();
+				}
+				else System.exit(1);
+			}
+		} catch(NullPointerException e){
+			System.err.println("No certain directory");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Wrong Format and directory");
+			System.exit(1);
+		}
+				
+		 //print for checking
 		for( String key : optsList.keySet() ){
             System.out.println( String.format("option : %s, args : %s", key, optsList.get(key)) );
         }
-
 	}
 	
+	//test if the options are valid. 
+	//the works for valid options are done in other section. 
 	private static boolean validOption(String arg){
-		if(arg.length() > 2)
+		if(arg.length() > 2 )
 			return false;
-		switch(arg.toLowerCase().charAt(1)){
+		switch(arg.charAt(1)){
 			case 'o':
 			case 'f':
 			case 's':
@@ -76,5 +124,5 @@ public class option_input {
 		}
 		return false;
 	}
-
+	
 }
