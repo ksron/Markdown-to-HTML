c5c9b03c2f5efb506ce4a35b1e14f7323641c9cb
ShinhaeKim <shinhae1106@gmail.com>
2016-11-19 06:01:59 +0900

    header, link address

--- Link_Addr.java
+++ Link_Addr.java
@@ -1,17 +1,19 @@
 
 public class Link_Addr extends Node{
 
-	protected String id;
-	protected String path_text;
-	protected String title;
+	public String id;
+	public String path_text;
+	public String title;
 	
 	public Link_Addr(String input_str)
 	{
 		super(input_str);
-	}
-	
-	public String generate()
-	{
-		return new String("");
+		
+		id=input_str.substring(input_str.indexOf("[")+1,input_str.indexOf("]"));
+		
+		String[] img_info_list=input_str.split(" ");
+		
+		path_text=img_info_list[1];
+		title=img_info_list[2].substring(1, img_info_list[2].length()-1);
 	}
 }
--- MDParser.java
+++ MDParser.java
@@ -1,6 +1,6 @@
 
 public class MDParser {
-
+	
 	public static String checkNode(String input_str)
 	{
 		return "Type name of a certain node";
