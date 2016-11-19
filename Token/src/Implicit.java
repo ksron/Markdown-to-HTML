0e279ddfc11291353e396141a8960662fbf0e409
DaheeCha_Joy <ekgmlekgml20@naver.com>
2016-11-19 14:33:26 +0900

    developing Link

--- Token/src/Implicit.java
+++ Token/src/Implicit.java
@@ -1,6 +1,22 @@
+//developing
+
 class Implicit extends Links
 {
 	public Implicit(String text_token) {
 		super(text_token);
+		
+		int i = text_token.indexOf("[");
+		int j = text_token.indexOf("]");
+	}
+	
+	//Todo
+	public String generate()
+	{
+		
+		return string_text;
 	}
 }
+
+
+
+
