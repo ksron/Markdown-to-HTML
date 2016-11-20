35e4a97b1db952ea08adaff770c3d0c6cddf1da9
DaheeCha_Joy <ekgmlekgml20@naver.com>
2016-11-20 15:18:58 +0900

    complete Inline.java

--- Token/src/Inline.java
+++ Token/src/Inline.java
@@ -1,15 +1,38 @@
+import org.w3c.dom.Text;
+
 //developing
 
 class Inline extends Links
 {
+	private String link;
+	//done
 	public Inline(String text_token) {
 		super(text_token);
+		
+		int i1 = text_token.indexOf("[");
+		int i2 = text_token.indexOf("]");
+		
+		int j1 = text_token.indexOf("(");
+		int j2 = text_token.indexOf(")");
+		
+		String link_text = text_token.substring(i1+1, i2);
+		
+		//link_url is absolute pathname or relative pathname of an website or local resource
+		String link_url = text_token.substring(j1+1, j2);
+		
+		if (link_url.contains("\""))
+		{
+			int k1 = text_token.indexOf("\"");
+			int k2 = text_token.lastIndexOf("\"");
+		    String title = text_token.substring(k1+1, k2);
+		    link = "<a href= \"" + link_url + "\" title = \""+title+"\">"+link_text+"</a>";
+		}
+
+		link = "<a href= \"" + link_url + "\">"+link_text+"</a>";
 	}
 	
-	//Todo
 	public String generate()
 	{
-		//To Be Modified
-		return string_text;
+		return link;
 	}
 }
