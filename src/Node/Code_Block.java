b22cb5366775577b3a38165e6dbd8190668dd436
moojoo <kasrlmoojoo@gmail.com>
2016-12-07 01:15:09 +0900

    Major fix
    fixed s.t inner string prints out as it is

--- src/Node/Code_Block.java
+++ src/Node/Code_Block.java
@@ -4,14 +4,31 @@ import util.Lines;
 
 class Code_Block extends Block{
 	
+	Lines codeLines = new Lines();
+	
 	public Code_Block(Lines lines)
 	{
-		super(lines, "<pre><code>", "</code></pre>");
+		super("<pre><code>", "</code></pre>");
 
 		for(int i = 0; i < lines.getLineNum(); i++){
 			lines.setLine(i, lines.lineAt(i).replaceFirst("([ ]{4}|\t)", ""));
+			lines.setLine(i, lines.lineAt(i).replaceAll("&", "&amp;"));
+			lines.setLine(i, lines.lineAt(i).replaceAll("<", "&lt;"));
+			lines.setLine(i, lines.lineAt(i).replaceAll(">", "&lt;"));
 		}
 		
-		tokenize(lines.toString());
+		codeLines = lines;
+	}
+	
+	public String generate()
+	{
+		for(int i = 0; i < codeLines.getLineNum(); i++)
+		{
+			html += codeLines.lineAt(i);
+		}
+		
+		html = html.trim();
+		
+		return startingTag+html+endingTag + "\n";
 	}
 }
