8c03e52edf24dcae698c1fadd9ab1d53a8c5dc8b
moojoo <kasrlmoojoo@gmail.com>
2016-12-06 21:48:05 +0900

    fixed for info hiding

--- src/mdconverter/Document.java
+++ src/mdconverter/Document.java
@@ -12,20 +12,20 @@ public class Document implements MDElement{
 	private String output_dir;
 	private String style;
 	public String converter;
-	
+
 	protected LinkedList<TokenComponent> nodes;
 
-	
+
 	public Document(){
-	
+
 	}
-		
+
 	public Document(Option options){
-		this.setInputFile(options.get("i"));
-		this.setOutputFile(options.get("o"));
-		this.setFormat(options.get("f"));
-		this.setStyle(options.get("s"));
-		this.setOutputDir(options.get("d"));
+		this.input_file = options.get("i");
+		this.output_file = options.get("o");
+		this.format = options.get("f");
+		this.style = options.get("s");
+		this.output_dir = options.get("d");
 		nodes = new LinkedList<TokenComponent>();
 	}
 
@@ -39,59 +39,30 @@ public class Document implements MDElement{
 
 	public String generate(){
 		String s ="";
-		
+
 		Iterator<TokenComponent> it = nodes.iterator();
 		while(it.hasNext()){
-			s += it.next().generate() + "\n";	
+			s += it.next().generate() + "\n";
 		}
-		
+
 		return s;
 	}
-	
+
 	//getter
 	public String getInputFile() {
 		return input_file;
 	}
-
-	public String getFormat() {
-		return format;
-	}
-
+	
 	public String getOutputFile() {
-		return output_file;
-	}
-
-	public String getOutputDir() {
-		return output_dir;
+		return output_dir + "\\" + output_file + "\\." + format;
 	}
 
 	public String getStyle() {
 		return style;
 	}
-	
-	//setter
-	private void setFormat(String format) {
-		this.format = format;
-	}
-
-	private void setOutputFile(String output_file) {
-		this.output_file = output_file;
-	}
-
-	private void setOutputDir(String output_dir) {
-		this.output_dir = output_dir;
-	}
-
-	private void setStyle(String style) {
-		this.style = style;
-	}
-
-	private void setInputFile(String input_file) {
-		this.input_file = input_file;
-	}
 
 	public void accept(MDElementVisitor md){
-		md.visit(this);	
+		md.visit(this);
 	}
 
 }
